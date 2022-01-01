9b4e4a6ef86e1d4eeef27c40d8a8a1fc8666906a
JaeHyeong Park <csp00275@skku.edu>
2022-01-01 18:18:33 +0900

    2nd commit

--- PyQt5/exam01.py
+++ PyQt5/exam01.py
@@ -11,7 +11,7 @@ class Exam(QWidget):
         btn.resize(btn.sizeHint())
         # btn.setToolTip('툴팁입니다.<b>안녕하세요<b/>')
         btn.move(50, 50)
-        btn.clicked.connect(QCoreApplication.instance().quit)
+        btn.clicked.connect(QCoreApplication.instance().quit) # 버튼 누르면 창 꺼짐
         #btn.center
 
 
@@ -19,8 +19,15 @@ class Exam(QWidget):
         self.setWindowTitle('두 번째 시간')
 
         self.show()
+    ######## 이벤트 연결 방법 ###########
     def closeEvent(self, QCloseEvent):
-        QMessageBox.question()
+        ans = QMessageBox.question(self,"종료 확인","종료하시겠습니까?",
+                                   QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+        if ans == QMessageBox.Yes:
+            QCloseEvent.accept() # CloseEvent 실행
+        else:
+            QCloseEvent.ignore() # CloseEvent 무시
+
 
 app = QApplication(sys.argv)
 w = Exam()
